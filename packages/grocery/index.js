a8769b0c77a532d30dbe405998c30d92b492a6e2
Hoa Nguyen Thai <hoa.nguyen-thai@lazada.com>
2018-11-30 10:24:20 +0700

    init

---
+++ packages/grocery/index.js
@@ -0,0 +1,8 @@
+const sillyname = require('sillyname');
+const apple = require('apple');
+const banana = require('banana');
+
+console.log(apple);
+console.log(banana);
+
+console.log(`grocery and ${sillyname()}`);
